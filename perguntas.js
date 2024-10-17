2d4e4b99aba863bf99c2a1598409c775f708608e
Figurante <133012688+VictorToshiharu@users.noreply.github.com>
2024-10-17 07:32:35 -0300

    nv 3

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma Linguagem de Programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França',
+    'A capital da Françca é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua Inglesa',
+    'Como se diz oi em Ingles?',
+    'Oi em Ingles é Hi (RAI)'
+)
